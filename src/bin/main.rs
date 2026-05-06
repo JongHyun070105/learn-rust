1b1192e1c58658a21548a992bfc59b7d44b1aba4
JongHyun070105 <bobi09361@gmail.com>
2026-05-06 20:19:50 +0900

    doc: std::env 라이브러리를 통해 입력 받기

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,14 @@
-fn main(){ // 기본: 리턴 타입이 없다. | 파라미터로 받는 것이 없다. -> 매우 간단한 형태!!
-    println!("hello")
+use std::env; // std::env 라이브러리를 사용하겠다는 의미
+
+// 기본: 리턴 타입이 없다. | 파라미터로 받는 것이 없다. -> 매우 간단한 형태!!
+fn main(){ 
+    let args: Vec<String> = env::args().collect(); // env::args().collect() 를 사용해서 입력값을 받는다.
+
+    let src_file = &args[1];
+    let tgt_file = &args[2];
+
+    println!("Source File: {}", src_file);
+    println!("Target File: {}", tgt_file);
+
+    // println!("hello")
 }
