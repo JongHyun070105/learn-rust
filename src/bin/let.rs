8d536670af939e976bf21066f4c73e97a5dcbe15
JongHyun070105 <bobi09361@gmail.com>
2026-05-08 20:32:11 +0900

    doc: print문 추가

--- src/bin/let.rs
+++ src/bin/let.rs
@@ -1,5 +1,9 @@
 fn main(){
     // let 변수명: 타입 = 값;
-    let name : &str; 
+    let name : &str = "홍길동"; 
     let age : i32;   // 초깃값 지정없이 생성
+
+    age = 20;
+
+    println!("이름: {}, 나이: {}",name, age);
 }
